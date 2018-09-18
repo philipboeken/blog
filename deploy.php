196b2ac2ad1d0d3c1c1d404a3b2eac575eb5d943
pabk <philip.boeken@hotmail.nl>
2018-09-19 01:24:22 +0200

    composer update

--- deploy.php
+++ deploy.php
@@ -38,4 +38,4 @@ after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
 
-// before('deploy:symlink', 'artisan:migrate');
+before('deploy:symlink', 'artisan:migrate');
