cd07f8293f8056525e79fd6efbc92eeb0dbcdd79
pabk <philip.boeken@hotmail.nl>
2018-09-19 00:04:07 +0200

    Initial commit for first deployment.

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'jlblog');
+
+// Project repository
+set('repository', 'git@github.com:philipboeken/blog.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+// Hosts
+
+host('178.62.225.66')
+    ->user('deployer')
+    ->identityFile('~/.ssh/deployerkey')
+    ->set('deploy_path', '/var/www/html/jlblog');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+// before('deploy:symlink', 'artisan:migrate');
